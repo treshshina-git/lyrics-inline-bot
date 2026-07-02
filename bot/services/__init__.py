675b5ade73975451bf20f4004c4631c28542ebf4
treshshina-git <treshshina@gmail.com>
2026-07-03 00:56:36 +0200

    3

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -2,14 +2,3 @@
 External services layer.
 """
 
-from bot.services.cache import TTLCache
-from bot.services.genius import genius_service
-from bot.services.http_client import http_client
-from bot.services.rate_limiter import inline_rate_limiter
-
-__all__ = [
-    "TTLCache",
-    "genius_service",
-    "http_client",
-    "inline_rate_limiter",
-]
