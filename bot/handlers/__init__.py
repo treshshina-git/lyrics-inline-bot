d979e0a96ed2d33268c2ed1749cf60ddfa7b1fbf
treshshina-git <treshshina@gmail.com>
2026-07-03 00:54:56 +0200

    2

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,19 +1,3 @@
 """
 Handlers layer.
 """
-
-from bot.handlers.error import error_handler
-from bot.handlers.help import help_handler
-from bot.handlers.health import health_handler
-from bot.handlers.inline import inline_query_handler
-from bot.handlers.start import start_handler
-from bot.handlers.version import version_handler
-
-__all__ = [
-    "inline_query_handler",
-    "start_handler",
-    "help_handler",
-    "health_handler",
-    "version_handler",
-    "error_handler",
-]
