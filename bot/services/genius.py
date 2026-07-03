6ae6378e4c73443a08271243e1b922cf38cafb06
treshshina-git <treshshina@gmail.com>
2026-07-03 04:39:05 +0200

    1

--- bot/services/genius.py
+++ bot/services/genius.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import httpx
+import httpx, json
 
 from bot.config import settings
 from bot.models.song import Song
@@ -34,10 +34,7 @@ class GeniusAPI:
                 Song(
                     id=result.get("id", 0),
                     title=result.get("title", ""),
-                    lyrics=await get_lyrics(
-                        artist=result.get("primary_artist", {}).get("name", ""),
-                        title=result.get("title", "")
-                    ),
+                    lyrics=None,
                     artist=result.get("primary_artist", {}).get("name", ""),
                     url=result.get("url", ""),
                     thumbnail=result.get("song_art_image_thumbnail_url"),
@@ -46,13 +43,14 @@ class GeniusAPI:
 
         return results
 
-    async def get_song(self, song_id: int) -> Song | None:
+    async def get_song_lrc(self, title: str, artist: str) -> str | None:
         async with httpx.AsyncClient(timeout=10) as client:
-            get_lyrics_result = await get_lyrics(
-                artist="Denzel Curry",
-                title="ULTIMATE"
-            )
-            data = get_lyrics_result.json()
+            lyrics = await get_lyrics(artist=artist, title=title)
+
+            print(f"get_song_lrc: {title} - {artist} -> {lyrics}")
+            #lyrics.raise_for_status()
+            #data = lyrics.json()
+
             #r = await client.get(
             #    f"{self.base_url}/songs/{song_id}",
             #    headers=self.headers,
@@ -60,20 +58,12 @@ class GeniusAPI:
             #r.raise_for_status()
             #data = r.json()
 
-        song = data.get("response", {}).get("song")
-        if not song:
-            return None
+            song = lyrics
+            if not song:
+                return None
 
-        return Song(
-            id=song.get("id", 0),
-            title=song.get("title", ""),
-            lyrics=get_lyrics_result,
-            artist=song.get("primary_artist", {}).get("name", ""),
-            url=song.get("url", ""),
-            thumbnail=song.get("song_art_image_thumbnail_url"),
-            album=song.get("album", {}).get("name") if song.get("album") else None,
-            release_date=song.get("release_date_for_display"),
-        )
+        return song.get("lyrics", {}).get("plain", None)
+    
 
 
 genius_api = GeniusAPI()
