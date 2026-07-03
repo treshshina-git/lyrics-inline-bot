748e05e73d08462e4d4483fc1f0dabe39fed80e8
treshshina-git <treshshina@gmail.com>
2026-07-03 07:11:35 +0200

    9

--- bot/utils/formatters.py
+++ bot/utils/formatters.py
@@ -2,22 +2,23 @@ from __future__ import annotations
 
 from bot.models.song import Song
 from bot.utils.text import safe_description, safe_title
-from bot.services.genius import genius_api
-
+#from bot.services.genius import genius_api
+from bot.services.lrclib import get_lyrics
 async def format_song_inline(song: Song) -> str:
 
     title = safe_title(song.title)
     artist = safe_description(song.artist)
+
+    lyricser = await get_lyrics(artist, title)
     print(f"format_song_inline: {title} - {artist} -> lyricser")
-    #lyricser = await genius_api.get_song_lrc(title, artist)
     #if lyricser is None:
     #    lyricser = "Lyrics not found"
     #else:
     #    print(f"format_song_inline: {lyricser}")
     #    lyricser = lyricser.strip()
         
-    #lyrics = lyricser if lyricser else "Lyrics not found"
-    lyrics = song.lyrics if song.lyrics else "Lyrics not found"
+    lyrics = lyricser if lyricser else "Lyrics not found"
+    #lyrics = song.lyrics if song.lyrics else "Lyrics not found"
     parts = [
         f"<b>{title}</b>",
         f"👤 {artist}",
