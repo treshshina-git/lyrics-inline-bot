0e1af6c0e0edaaff06c1e66dc624606d3cb819d9
treshshina-git <treshshina@gmail.com>
2026-07-03 06:31:50 +0200

    1

--- bot/utils/formatters.py
+++ bot/utils/formatters.py
@@ -5,11 +5,12 @@ from bot.utils.text import safe_description, safe_title
 from bot.services.genius import genius_api
 
 async def format_song_inline(song: Song) -> str:
-    print(f"format_song_inline: {song.title} - {song.artist} -> {song.id}")
+
     title = safe_title(song.title)
     artist = safe_description(song.artist)
     lyricser = await genius_api.get_song_lrc(title, artist)
     lyrics = str(lyricser) if lyricser else "Lyrics not found"
+    print(f"format_song_inline: {title} - {artist} -> {lyrics}")
     parts = [
         f"<b>{title}</b>",
         f"👤 {artist}",
@@ -21,7 +22,7 @@ async def format_song_inline(song: Song) -> str:
     #if song.release_date:
     #    parts.append(f"📅 {song.release_date}")
 
-    if song.lyrics:
+    if lyrics:
         parts.append(f"📝 {lyrics}")
 
     #if song.url:
